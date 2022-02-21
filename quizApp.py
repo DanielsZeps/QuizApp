10c99ef6471305aae4847c167a50d85e51b104e3
DanielsZeps <77250340+DanielsZeps@users.noreply.github.com>
2022-02-21 10:41:07 +0200

    Add files via upload

---
+++ quizApp.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+@app.route("/style")
+def style():
+    return render_template("style.css")
+
+@app.route("/script")
+def script():
+    return render_template("script.js")
+
+@app.route("/textResponse")
+def textResponse():
+    return render_template("text.txt")
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug="true")
