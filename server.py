9605ae12b757654a45ea5db1871579c5edc3d4a7
DanielsZeps <77250340+DanielsZeps@users.noreply.github.com>
2022-03-28 10:00:24 +0300

    asd
    asd

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import *
+
+app = Flask(__name__)
+
+#Pirmā lapa, kas tiks ielādēta
+@app.route('/',methods = ['POST', 'GET'])
+def root():
+    return render_template("index.html")
+    
+#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
+@app.route('/health')
+def health():
+  return "OK"
+
+@app.route('/about')
+def about():
+  return render_template("about.html")
+
+@app.route('/test')
+def test():
+  parametri = ["Augums", "Svars", "Vecums"]
+  return render_template("test.html", param = parametri)
+
+if __name__ == '__main__':
+  app.run(debug="true")
